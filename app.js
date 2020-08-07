a7aeded3da98f5519c4d21b76668f88d2ab1a629
smartmelvin <69347106+smartmelvin@users.noreply.github.com>
2020-08-07 16:08:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDuk-NgO8yptlVJpaB9uO2TSUFSxw4WrPU",
+  authDomain: "testing-323a3.firebaseapp.com",
+  databaseURL: "https://testing-323a3.firebaseio.com",
+  projectId: "testing-323a3",
+  storageBucket: "testing-323a3.appspot.com",
+  messagingSenderId: "92375521719",
+  appId: "1:92375521719:web:a4723e1199a348b9f3f032",
+  measurementId: "G-F9WTRGMR9F"
 };
-
-     
  
 ////Do Not Edit Below This
 
